b3df0f00a90b20c68558f97f09f545f91672af60
Chely Ho <adept_inept@yahoo.com>
2018-07-28 23:06:10 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -108,7 +108,8 @@ function previousGames() {
     })
   })
   listing.done(function() {
-    $('#games button').on('click', loadGame(this))
+    $('#games button').on('click', function() {
+      loadGame(this))
   })
 }
 
